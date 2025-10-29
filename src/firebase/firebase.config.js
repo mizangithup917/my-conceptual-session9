e718a500ece6b27d6416695bd843035ab502dd8e
Mizna PC <mizanur.wp2024@gmail.com>
2025-10-29 23:33:14 +0600

    my conceptual session9 therd comment

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,25 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-import { getAuth } from "firebase/auth";
-
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyABWddYGFzbKtaUmVes8JJEDOH5QMy1hLY",
   authDomain: "my-conceptual-session9.firebaseapp.com",
   projectId: "my-conceptual-session9",
-  storageBucket: "my-conceptual-session9.appspot.com", // ✅ ঠিক করা হলো
+  storageBucket: "my-conceptual-session9.firebasestorage.app",
   messagingSenderId: "512723641151",
   appId: "1:512723641151:web:08df2a4daaf2ecd234698a"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app);
-
+export const auth =  getAuth(app);
--- src/main.jsx
+++ src/main.jsx
@@ -3,25 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router/dom";
 import { router } from "./routes/routes.jsx";
-
-// ✅ Toastify import করো
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <>
-      <RouterProvider router={router} />
-      {/* ✅ ToastContainer নিচে রাখো */}
-      <ToastContainer
-        position="top-center"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        pauseOnHover
-        theme="colored"
-      />
-    </>
+      <RouterProvider router={router}/>
+      <ToastContainer/>
   </StrictMode>
 );
